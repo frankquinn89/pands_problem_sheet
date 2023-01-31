a4802fcf1604542c42ebdd31aabdc55536998966
Frank <G00340834@atu.ie>
2023-01-31 18:08:06 +0000

    Reoving semicolons in code

--- week02/bank.py
+++ week02/bank.py
@@ -2,10 +2,10 @@
 # This program outputs the sum of 2 inputs (in cents) into Euro format.
 # Author: Frank Quinn
 
-amount1 = input("Enter amount 1 (in cents) : ");
-amount2 = input("Enter amount 2 (in cents) : ");
+amount1 = input("Enter amount 1 (in cents) : ")
+amount2 = input("Enter amount 2 (in cents) : ")
 
-totalCents = int(amount1) + int(amount2);
-totalEuro = totalCents/100;
+totalCents = int(amount1) + int(amount2)
+totalEuro = totalCents/100
 
-print(f"The sum of {amount1} cents and {amount2} cents is : €{totalEuro}");
+print(f"The sum of {amount1} cents and {amount2} cents is : €{totalEuro}")
