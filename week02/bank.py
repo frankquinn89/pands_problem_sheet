b9af003a4d6c12e3776f48bdb07c17189743a6f9
Frank <G00340834@atu.ie>
2023-02-02 21:52:42 +0000

    add format to  decimals places

--- week02/bank.py
+++ week02/bank.py
@@ -6,6 +6,6 @@ amount1 = input("Enter amount 1 (in cents) : ")
 amount2 = input("Enter amount 2 (in cents) : ")
 
 totalCents = int(amount1) + int(amount2)
-totalEuro = totalCents/100
+totalEuro = format(totalCents/100, '.2f')
 
 print(f"The sum of {amount1} cents and {amount2} cents is : €{totalEuro}")
